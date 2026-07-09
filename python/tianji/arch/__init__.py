8172c2aeff9b698196349ac68888b791e6c430ef
lanmower <almagestfraternite@gmail.com>
2026-07-09 16:19:14 +0200

    add python/tianji/arch/__init__.py

---
+++ python/tianji/arch/__init__.py
@@ -0,0 +1,5 @@
+from .mamba2 import Mamba2Layer, MambaConfig
+from .mla import MLALayer, MLAConfig
+from .moe import MoELayer, MoEConfig
+from .hybrid import HybridStack, HybridConfig, RMSNorm, MambaBlock, MLAMoELayer
+from .mtp import MTPHead, MTPConfig
